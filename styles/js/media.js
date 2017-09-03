571a2116d8cf19069bff01f030f7d3f9b40d75cd
wenjie <939800704@qq.com>
2017-09-04 00:39:48 +0800

    1

--- styles/js/media.js
+++ styles/js/media.js
@@ -1,24 +1,23 @@
 
 window.onload = function(){
+	var list=[];
+	var ind=0;
+	var time=0;
 	var myAudio=document.getElementById("media");
-	var list=null;
 	if(myAudio){
 		list=$.map($("#media>source"),function(obj,i){return $(obj).attr('src');});
-		myAudio.volume=0.4;
+		myAudio.volume=0.8;
 		myAudio.loop = false;
 		myAudio.src = list[0];
 		myAudio.addEventListener("ended", playEndedHandler);
+		myAudio.addEventListener("timeupdate",timeupdateHandler);
 		myAudio.pause();
-		
-		function playEndedHandler(){
-			var index=list.indexOf(myAudio.src);
-			var len=list.length;
-			index=index+1;
-			index=index>len-1?0:index;
-			play(index);
-		}
+		checkMusic();
 		
 		function play(index){
+			ind=index;
+			time=0;
+			saveTime(true);
 			var $that=$('.play.glyphicon');
 			$that.addClass('glyphicon-pause');
 			$that.removeClass('glyphicon-play');
@@ -43,15 +42,61 @@ window.onload = function(){
 				$that.addClass('glyphicon-play');
 				$that.removeClass('glyphicon-pause');
 				myAudio.pause();
+				setTimeout(function(){saveTime(false);},500)
 			}else if($that.hasClass('glyphicon-play')){
 				$that.removeClass('glyphicon-play');
 				$that.addClass('glyphicon-pause');
 				myAudio.play();
 			}
-			
-			
-			
 		});
+		
+		function playEndedHandler(){
+			var index=list.indexOf(myAudio.src);
+			var len=list.length;
+			index=index+1;
+			index=index>len-1?0:index;
+			play(index);
+		}
+		
+		var his=0;
+		function timeupdateHandler(){
+			time = myAudio.currentTime;
+			if(time!=his){
+				his=time;
+				saveTime(true);
+			}
+        }
+		
+ 		function  checkMusic(){
+			var music = getTime();
+			if(music){
+				ind=music.index;
+				time=music.time;
+				myAudio.src = list[ind];
+				myAudio.preload="auto";
+				myAudio.currentTime = time;
+				if(music.isPlay){
+					var $that=$('.play.glyphicon');
+					$that.addClass('glyphicon-pause');
+					$that.removeClass('glyphicon-play');
+					myAudio.play();
+				}
+			}
+			
+ 		}
+ 		
+ 		function saveTime(isPlay){
+ 			var music = {index:ind,isPlay:isPlay,time:time};
+			var musicObj = JSON.stringify(music);
+			localStorage.setItem("music", musicObj);
+ 		}
+ 		
+ 		function getTime(){
+ 			var music = localStorage.getItem("music");
+			var musicObj = JSON.parse(music);
+			return musicObj;
+ 		}
 	}else{
+		
 	}
  }
