eca14411a54ede95287cecc35ff807cd468d7fee
wenjie <939800704@qq.com>
2017-09-04 10:24:53 +0800

    1

--- styles/js/media.js
+++ styles/js/media.js
@@ -1,9 +1,10 @@
+var list=[];
+var ind=0;
+var time=0;
+var his=0;
+var myAudio=$("#media")[0];
 
 window.onload = function(){
-	var list=[];
-	var ind=0;
-	var time=0;
-	var myAudio=document.getElementById("media");
 	if(myAudio){
 		list=$.map($("#media>source"),function(obj,i){return $(obj).attr('src');});
 		myAudio.volume=0.8;
@@ -13,90 +14,92 @@ window.onload = function(){
 		myAudio.addEventListener("timeupdate",timeupdateHandler);
 		myAudio.pause();
 		checkMusic();
-		
-		function play(index){
-			ind=index;
-			time=0;
-			saveTime(true);
+	}
+ }
+
+function play(index){
+	ind=index;
+	time=0;
+	saveTime(true);
+	var $that=$('.play.glyphicon');
+	$that.addClass('glyphicon-pause');
+	$that.removeClass('glyphicon-play');
+	myAudio.src = list[index];
+	myAudio.preload="auto";
+	myAudio.play();
+}
+
+$(".glyphicon").click(function(){
+	var index=list.indexOf(myAudio.src);
+	var len=list.length;
+	var $that=$(this);
+	if($that.hasClass('glyphicon-backward')){
+		index=index-1;
+		index=index<0?len-1:index;
+		play(index);
+	}else if($that.hasClass('glyphicon-forward')){
+		index=index+1;
+		index=index>len-1?0:index;
+		play(index);
+	}else if($that.hasClass('glyphicon-pause')){
+		$that.addClass('glyphicon-play');
+		$that.removeClass('glyphicon-pause');
+		myAudio.pause();
+		setTimeout(function(){saveTime(false);},500)
+	}else if($that.hasClass('glyphicon-play')){
+		$that.removeClass('glyphicon-play');
+		$that.addClass('glyphicon-pause');
+		myAudio.play();
+	}
+});
+
+function playEndedHandler(){
+	var index=list.indexOf(myAudio.src);
+	var len=list.length;
+	index=index+1;
+	index=index>len-1?0:index;
+	play(index);
+}
+
+function timeupdateHandler(){
+	time = myAudio.currentTime;
+	if(time!=his){
+		his=time;
+		saveTime(true);
+	}
+}
+
+function  checkMusic(){
+	var music = getTime();
+	if(music){
+		ind=music.index;
+		time=music.time;
+		his=time;
+		myAudio.src = list[ind];
+		myAudio.preload="auto";
+		myAudio.currentTime = time;
+		if(music.isPlay){
 			var $that=$('.play.glyphicon');
 			$that.addClass('glyphicon-pause');
 			$that.removeClass('glyphicon-play');
-			myAudio.src = list[index];
-			myAudio.preload="auto";
 			myAudio.play();
 		}
-		
-		$(".glyphicon").click(function(){
-			var index=list.indexOf(myAudio.src);
-			var len=list.length;
-			var $that=$(this);
-			if($that.hasClass('glyphicon-backward')){
-				index=index-1;
-				index=index<0?len-1:index;
-				play(index);
-			}else if($that.hasClass('glyphicon-forward')){
-				index=index+1;
-				index=index>len-1?0:index;
-				play(index);
-			}else if($that.hasClass('glyphicon-pause')){
-				$that.addClass('glyphicon-play');
-				$that.removeClass('glyphicon-pause');
-				myAudio.pause();
-				setTimeout(function(){saveTime(false);},500)
-			}else if($that.hasClass('glyphicon-play')){
-				$that.removeClass('glyphicon-play');
-				$that.addClass('glyphicon-pause');
-				myAudio.play();
-			}
-		});
-		
-		function playEndedHandler(){
-			var index=list.indexOf(myAudio.src);
-			var len=list.length;
-			index=index+1;
-			index=index>len-1?0:index;
-			play(index);
-		}
-		
-		var his=0;
-		function timeupdateHandler(){
-			time = myAudio.currentTime;
-			if(time!=his){
-				his=time;
-				saveTime(true);
-			}
-        }
-		
- 		function  checkMusic(){
-			var music = getTime();
-			if(music){
-				ind=music.index;
-				time=music.time;
-				myAudio.src = list[ind];
-				myAudio.preload="auto";
-				myAudio.currentTime = time;
-				if(music.isPlay){
-					var $that=$('.play.glyphicon');
-					$that.addClass('glyphicon-pause');
-					$that.removeClass('glyphicon-play');
-					myAudio.play();
-				}
-			}
-			
- 		}
- 		
- 		function saveTime(isPlay){
- 			var music = {index:ind,isPlay:isPlay,time:time};
-			var musicObj = JSON.stringify(music);
-			localStorage.setItem("music", musicObj);
- 		}
- 		
- 		function getTime(){
- 			var music = localStorage.getItem("music");
-			var musicObj = JSON.parse(music);
-			return musicObj;
- 		}
-	}else{
-		
 	}
- }
+	
+}
+
+function saveTime(isPlay){
+	if(window.localStorage){
+		var music = {index:ind,isPlay:isPlay,time:time};
+		var musicObj = JSON.stringify(music);
+		localStorage.setItem("music", musicObj);
+	}
+}
+
+function getTime(){
+	if(window.localStorage){
+		var music = localStorage.getItem("music");
+		var musicObj = JSON.parse(music);
+		return musicObj;
+	}
+}
